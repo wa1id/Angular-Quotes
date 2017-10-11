38ab8d3aa439768751b0db5ac22783f2569b8000
Unknown <wyachou95@gmail.com>
2017-10-11 20:15:05 +0200

    Show hardcoded persons on our page

--- app/app.js
+++ app/app.js
@@ -2,29 +2,42 @@
 
 angular.module('App', ['ngRoute'])
 
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/home', {
-      templateUrl: 'assets/views/home.html',
-      controller: 'homeCtrl'
-    })
-    .when('/persons', {
-      templateUrl: 'assets/views/persons.html',
-      controller: 'personsCtrl'
-    })
-    .otherwise({
-      redirectTo: '/home'
-    });
-})
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/home', {
+        templateUrl: 'assets/views/home.html',
+        controller: 'homeCtrl'
+      })
+      .when('/persons', {
+        templateUrl: 'assets/views/persons.html',
+        controller: 'personsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/home'
+      });
+  })
 
-.config(function($compileProvider) {
-  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|sms):/);
-})
+  .config(function($compileProvider) {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|sms):/);
+  })
 
-.controller('homeCtrl', ['$scope', function homeCtrl($scope) {
+  .controller('homeCtrl', ['$scope', function homeCtrl($scope) {}])
 
-}])
+  .controller('personsCtrl', ['$scope', 'personSrv', function personsCtrl($scope, personSrv) {
+    $scope.persons = personSrv.getAllPersons();
+  }])
 
-.controller('personsCtrl', ['$scope', function personsCtrl($scope) {
-
-}])
+  .factory('personSrv', [function() {
+    var persons = [{
+      'id': 0,
+      'name': 'Bill Gates'
+    }, {
+      'id': 1,
+      'name': 'Steve Jobs'
+    }];
+    return {
+      getAllPersons: function() {
+        return persons;
+      }
+    }
+  }])
